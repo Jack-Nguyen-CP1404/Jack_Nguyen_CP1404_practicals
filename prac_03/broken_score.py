8be86860bf82f422ad579d6e38c93fa042028d49
Jack Nguyen <68810480+jackoyo@users.noreply.github.com>
2020-08-16 20:54:56 +1000

    Write function to determine score status

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -3,15 +3,31 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-# Use if statement to determine score status
-if score < 0:
-    print("Invalid score")
-elif 0 < score < 50:
-    print("Bad")
-elif 50 <= score < 90:
-    print("Passable")
-elif 90 <= score <= 100:
-    print("Excellent")
-else:
-    print("Invalid score")  # if score is greater than 100 or less than 0 then print invalid score
+
+# import random
+
+
+def main():
+    score = float(input("Enter score: "))
+    print("Your score status is: {}".format(score_status(score)))
+
+
+# score = random.randint(0, 100)
+# print("Score {} is: {}".format(score, score_status(score)))
+
+
+def score_status(score):
+    # Use if statement to determine score status
+    if score < 0:
+        return "Invalid"
+    elif 0 < score < 50:
+        return "Bad"
+    elif 50 <= score < 90:
+        return "Passable"
+    elif 90 <= score <= 100:
+        return "Excellent"
+    else:
+        return "Invalid score"  # if score is greater than 100 or less than 0 then print invalid score
+
+
+main()
