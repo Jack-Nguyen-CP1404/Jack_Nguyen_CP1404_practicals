498018dc09ed950a5e518613a6d0a5b0a490675d
Jack Nguyen <68810480+jackoyo@users.noreply.github.com>
2020-08-20 15:38:33 +1000

    final versions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,6 +1,34 @@
 PASSWORD_LENGTH = 5
-password = input("Enter the password: ")
-if len(password) <= PASSWORD_LENGTH:
-    print(("*" * len(password)))
-else:
-    print(("Your password is: {}".format(password)))
+
+
+def password_checker():
+    """Get a password of valid size and print asterisks."""
+    password = input("Enter password of at least {} characters: ".format(PASSWORD_LENGTH))
+    while len(password) < PASSWORD_LENGTH:
+        password = input("Enter password of at least {} characters: ".format(PASSWORD_LENGTH))
+
+    print('*' * len(password))
+
+
+password_checker()
+
+
+# Break down functions
+
+def main():
+    """Print asterisks."""
+    password = password_check(PASSWORD_LENGTH)
+    print('*' * len(password))
+
+
+def password_check(password_length):
+    """Check password length"""
+    password = input("Enter password of at least {} characters: ".format(password_length))
+    while len(password) < password_length:
+        print("Your pass word is too short")
+        password = input("Enter password of at least {} characters: ".format(password_length))
+
+    return password
+
+
+main()
