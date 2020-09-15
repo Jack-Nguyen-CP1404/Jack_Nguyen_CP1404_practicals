4f9af94fbcd99867a2acfd8cea91741bf04679b6
Jack Nguyen <68810480+jackoyo@users.noreply.github.com>
2020-09-16 09:43:53 +1000

    Create new event handlers to allow user to input and display to output or clear data

--- prac_07/box_layout_demo.py
+++ prac_07/box_layout_demo.py
@@ -9,7 +9,7 @@ class BoxLayoutDemo(App):
         return self.root
 
     def handle_greet(self):
-        """Create a handle_greet function to change text Label"""
+        """Create a handle_greet function to change text Label."""
         print('greet')
         print("test")
         # Change text label
@@ -17,7 +17,7 @@ class BoxLayoutDemo(App):
         self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
 
     def handle_clear_data(self):
-        """Create a clear data even handler to clear input and output to blank"""
+        """Create a clear data even handler to clear input and output to blank."""
         self.root.ids.output_label.text = ""
         self.root.ids.input_name.text = ""
 
