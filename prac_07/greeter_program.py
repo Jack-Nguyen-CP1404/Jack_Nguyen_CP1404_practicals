62298ba438b1f98084cb65ed8b7fa97be6e5216a
Jack Nguyen <68810480+jackoyo@users.noreply.github.com>
2020-09-19 12:04:42 +1000

    Greeter program in different layout

---
+++ prac_07/greeter_program.py
@@ -0,0 +1,25 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class BoxLayoutDemo(App):
+    def build(self):
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('greeter_program.kv')
+        return self.root
+
+    def handle_greet(self):
+        """Create a handle_greet function to change text Label."""
+        print('greet')
+        print("test")
+        # Change text label
+        # self.root.ids.output_label.text = "Hello "
+        self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
+
+    def handle_clear_data(self):
+        """Create a clear data even handler to clear input and output to blank."""
+        self.root.ids.output_label.text = ""
+        self.root.ids.input_name.text = ""
+
+
+BoxLayoutDemo().run()
