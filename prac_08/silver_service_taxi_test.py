106c77e8b12641fdb9580e91839eb7b11d71ca3f
Jack Nguyen <68810480+jackoyo@users.noreply.github.com>
2020-10-04 11:34:11 +1000

    Test function for class SilverServiceTaxi

---
+++ prac_08/silver_service_taxi_test.py
@@ -0,0 +1,15 @@
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    """Test function for class SilverServiceTaxi"""
+    fancy_taxi_1 = SilverServiceTaxi("Hummer taxi", 200, 4)
+    print(fancy_taxi_1)
+    # Test for 18km trip with fancy of 2
+    fancy_taxi_2 = SilverServiceTaxi("BMW", 100, 2)
+    fancy_taxi_2.drive(18)
+    print(fancy_taxi_2)
+    print(fancy_taxi_2.get_fare())
+
+
+main()
